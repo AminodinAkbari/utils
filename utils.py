9c5775af3409f2e046e93e800a5273d7b1eab425
Mr.AminodinAkbari <95363362+AminodinAkbari@users.noreply.github.com>
2022-03-19 19:45:31 +0330

    Create utils.py
    (calculate time for views in django)

---
+++ utils.py
@@ -0,0 +1,12 @@
+#this function can use as a decorator help u to get how many times spend to get queries from DB
+def debugger(func):
+    def wrapper(*args,**kwargs):
+        reset_queries()
+        start_time = time.time()
+        value = func(*args,**kwargs) 
+        end_time = time.time()
+        queries = len(connection.queries)
+        print(f"\n-------------\nConection Numbers: {queries} \n taketime = {(end_time-start_time):.3f}\n-----------\n")
+        return value
+    return wrapper
+#***********************************************************************************************
